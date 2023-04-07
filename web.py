7d25d25d8356190fef390d2784777679547d003c
srinmuz <srinmuz@gmail.com>
2023-04-07 12:01:21 +0000

    commited back

--- web.py
+++ web.py
@@ -2,12 +2,10 @@ import streamlit as st
 import functions
 
 todos = functions.get_todos()
-
 def add_todo():
     todo = st.session_state["new_todo"] + '\n'
     todos.append(todo)
     functions.write_todos(todos)
-
 
 st.title("My Todo App")
 st.subheader("This is my todo app")
@@ -20,8 +18,6 @@ for index,todo in enumerate(todos):
         functions.write_todos(todos)
         del st.session_state[todo]
         st.experimental_rerun()
-
-
 
 st.text_input(label=" ", placeholder="Add New todo...",
               on_change=add_todo, key='new_todo')
